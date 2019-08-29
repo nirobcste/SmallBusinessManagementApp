0a3f50d325cd3e1d74b89f3a1c7556a7c84d3960
Your Name <nizamuddinnstu@gmail.com>
2019-08-29 12:06:14 +0600

    first commit

---
+++ SmallBusinessManagementApp.Models/Models/Purchase/PurchaseDetails.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SmallBusinessManagementApp.Models.Models.Purchase
+{
+   public class PurchaseDetails
+    {
+       public int Id { get; set; }
+    
+       public string Code { get; set; }
+
+       [DisplayName("Manufactured Date")]
+       public string ManufacturedDate { get; set; }
+
+       [DisplayName("Expired Date")]
+       public string ExpiredDate { get; set; }
+
+       [DisplayName("Purchase Quantity")]
+       public int PurchaseQuantity  { get; set; }
+       [DisplayName("Unit Price")]
+       public int UnitPrice { get; set; }
+       [DisplayName("Total Price")]
+       public int TotalPrice { get; set; }
+       [DisplayName("Previous MRP")]
+       public int PreviousMRP { get; set; }
+       public int PurchaseId { get; set; }
+
+       [DisplayName("New MRP")]
+       public int NewMRP { get; set; }
+       public int ProductId { get; set; }
+       public virtual Product Product { get; set; }
+       public virtual Purchase Purchase { get; set; }
+
+       
+
+       
+    }
+}
